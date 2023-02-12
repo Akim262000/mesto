d9e845c7f86165a6fe90facb36724aecb127d783
Akim Minko <akim.minko@yandex.ru>
2023-02-12 17:31:58 +0300

    feat: сделать popup с кнопками

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,41 @@
-
+// Находим popup
 let popup = document.querySelector('.popup');
-let popupCloseButton = popup.querySelector('.popup__close');
+// Находим кнопку открытия popup
 let profileEditButton = document.querySelector('.profile__edit-button');
-
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+// Находим кнопку закрытия popup
+let popupCloseButton = popup.querySelector('.popup__close');
+// Находим форму в DOM
+let form = popup.querySelector('.popup__form');
+// Находим поля формы в DOM
+let nameInput = form.querySelector('#name');
+let jobInput = form.querySelector('#description');
+// Находим поля профиля
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+// Вносим данные в форму
+function addValue() {
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
 }
-
-profileEditButton.addEventListener('click', togglePopup);
-popupCloseButton.addEventListener('click', togglePopup);
+// Открываем popup
+function openPopup() {
+  popup.classList.add('popup_opened');
+  addValue();
+}
+// Закрываем popup
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+// Обработчики событий
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+// Обработчик отправки формы
+function submitFormHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+// Закрываем popup
+  closePopup();
+}
+// Прикрепляем обработчик к форме
+form.addEventListener('submit', submitFormHandler);
