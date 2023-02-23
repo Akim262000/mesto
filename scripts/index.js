616342dcbb7ae9bdc7c128b5af2c29c34e1672fc
Akim Minko <akim.minko@yandex.ru>
2023-02-23 19:04:04 +0300

    feat: сделать добавление карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,41 @@
+const cards = [
+  {
+    image: './images/Saint-Petersburg.jpg',
+    title: 'Санкт-Петербург'
+  },
+  {
+    image: './images/Rostov-on-Don.jpg',
+    title: 'Ростов-на-Дону'
+  },
+  {
+    image: './images/Elbrus.jpg',
+    title: 'Гора Эльбрус'
+  },
+  {
+    image: './images/Pervomayskiy.jpg',
+    title: 'п. Первомайский'
+  },
+  {
+    image: './images/Taganrog.jpg',
+    title: 'Таганрог'
+  },
+  {
+    image: './images/Kushevskaya.jpg',
+    title: 'ст. Кущевская'
+  }
+]
+
+const elements = document.querySelector('.elements');
+
+cards.forEach(function(card) {
+  const newCard = document.querySelector('#elementTemplate').content.cloneNode(true);
+  const cardTitle = newCard.querySelector('.element__title');
+  cardTitle.textContent = card.title;
+  const cardImage = newCard.querySelector('.element__image');
+  cardImage.setAttribute('src', card.image);
+  elements.append(newCard);
+})
+
 // Находим popup
 let popup = document.querySelector('.popup');
 // Находим кнопку открытия popup
@@ -12,6 +50,19 @@ let jobInput = form.querySelector('#description');
 // Находим поля профиля
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
+
+// let popupNewCard = document.querySelector('.popup_new_card')
+
+// let profileAddButton = document.querySelector('.profile__add-button');
+
+// // Находим поля формы в DOM
+// let titleInput = form.querySelector('#title');
+// let linkInput = form.querySelector('#image');
+// // Находим поля профиля
+// let elementTitle = document.querySelector('.element__title');
+// let elementImage = document.querySelector('.element__image');
+
+
 // Вносим данные в форму
 function addValue() {
   nameInput.value = profileName.textContent;
@@ -38,4 +89,5 @@ function submitFormHandler(evt) {
 form.addEventListener('submit', submitFormHandler);
 // Обработчики событий
 profileEditButton.addEventListener('click', openPopup);
+// profileAddButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
