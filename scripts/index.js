a8e8d47aea93ad6e3738a1538c94f9098162d87b
Akim Minko <akim.minko@yandex.ru>
2023-02-24 13:33:09 +0300

    feat: сделать добавление карточек из popup

--- scripts/index.js
+++ scripts/index.js
@@ -26,15 +26,27 @@ const cards = [
 ]
 
 const elements = document.querySelector('.elements');
+// Создаем элемент
+function createElement(element) {
+  const newElement = document.querySelector('#elementTemplate').content.cloneNode(true);
+  const elementTitle = newElement.querySelector('.element__title');
+  elementTitle.textContent = element.title;
+  const elementImage = newElement.querySelector('.element__image');
+  elementImage.setAttribute('src', element.image);
+  const deleteButton = newElement.querySelector('.element__button-delete');
+  deleteButton.addEventListener('click', deleteButtonClick);
+  elements.prepend(newElement);
+}
 
-cards.forEach(function(card) {
-  const newCard = document.querySelector('#elementTemplate').content.cloneNode(true);
-  const cardTitle = newCard.querySelector('.element__title');
-  cardTitle.textContent = card.title;
-  const cardImage = newCard.querySelector('.element__image');
-  cardImage.setAttribute('src', card.image);
-  elements.append(newCard);
-})
+cards.forEach(createElement); 
+// Удвляем елемент
+function deleteButtonClick(event) {
+// Находим элемент
+  const button = event.target;
+  const element = button.closest('.element');
+// Удаляем элемент
+  element.remove();
+}
 
 // Находим popup
 let popup = document.querySelector('.popup');
@@ -51,10 +63,25 @@ let jobInput = form.querySelector('#description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
-// let popupNewCard = document.querySelector('.popup_new_card')
 
-// let profileAddButton = document.querySelector('.profile__add-button');
 
+let popupNewCard = document.querySelector('.popup_new_card')
+
+let openAddButton = document.querySelector('.profile__add-button');
+
+const closeAddButton = popupNewCard.querySelector('.popup__close');
+
+const addNewElementButton = popupNewCard.querySelector('.popup__submit-button');
+
+const addElementForm = popupNewCard.querySelector('.popup__form');
+
+openAddButton.addEventListener('click',() => {
+  popupNewCard.classList.add('popup_opened');
+})
+
+closeAddButton.addEventListener('click',() => {
+  popupNewCard.classList.remove('popup_opened');
+})
 // // Находим поля формы в DOM
 // let titleInput = form.querySelector('#title');
 // let linkInput = form.querySelector('#image');
@@ -62,6 +89,18 @@ let profileDescription = document.querySelector('.profile__description');
 // let elementTitle = document.querySelector('.element__title');
 // let elementImage = document.querySelector('.element__image');
 
+popupNewCard.addEventListener('submit',handleFormSubmit)
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  form = event.target;
+  const title = form.querySelector('#title').value;
+  const image = form.querySelector('#image').value;
+  const element = {
+      title, image
+  }
+  createElement(element);
+}
 
 // Вносим данные в форму
 function addValue() {
@@ -89,5 +128,5 @@ function submitFormHandler(evt) {
 form.addEventListener('submit', submitFormHandler);
 // Обработчики событий
 profileEditButton.addEventListener('click', openPopup);
-// profileAddButton.addEventListener('click', openPopup);
+
 popupCloseButton.addEventListener('click', closePopup);
